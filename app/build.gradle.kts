91a5f995533b48aa376f096b4661d01d08261b5c
pseedk <pseedk@ya.ru>
2024-02-04 19:08:07 +0300

    New style

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs")
+    id("kotlin-parcelize")
+
 }
 
 android {
@@ -50,18 +52,42 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("com.intuit.ssp:ssp-android:1.1.0")
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+    implementation("com.github.yogacp:android-viewbinding:1.0.4")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    ksp("com.google.dagger:dagger-compiler:2.48.1")
-    ksp("com.google.dagger:hilt-compiler:2.48.1")
-
     implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
 
     implementation("io.coil-kt:coil:2.5.0")
 
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    implementation ("com.github.ybq:Android-SpinKit:1.4.0")
+    implementation ("com.flaviofaria:kenburnsview:1.0.7")
+
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-common-java8:2.7.0")
+
 }
--- app/src/main/java/ru/pvkovalev/wallpaperapp/ui/MainActivity.kt
+++ app/src/main/java/ru/pvkovalev/wallpaperapp/ui/MainActivity.kt
@@ -1,86 +1,19 @@
 package ru.pvkovalev.wallpaperapp.ui
 
-import android.app.Dialog
-import android.app.WallpaperManager
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
+import android.viewbinding.library.activity.viewBinding
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import androidx.paging.LoadState
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import ru.pvkovalev.wallpaperapp.R
 import ru.pvkovalev.wallpaperapp.databinding.ActivityMainBinding
-import ru.pvkovalev.wallpaperapp.databinding.DialogLoadingBinding
-import ru.pvkovalev.wallpaperapp.utils.SetWallpaperTask
-import ru.pvkovalev.wallpaperapp.viewmodel.UnsplashViewModel
-import javax.inject.Inject
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: UnsplashViewModel by viewModels()
-
-    @Inject
-    lateinit var adapter: UnsplashAdapter
-
-    private lateinit var binding: ActivityMainBinding
-
-    @Inject
-    lateinit var wallpaperManager: WallpaperManager
+    private val binding: ActivityMainBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        installSplashScreen()
         setContentView(binding.root)
-
-        binding.apply {
-            recyclerView.layoutManager =
-                StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-            recyclerView.adapter = adapter
-
-            swipeRefreshLayout.setOnRefreshListener {
-                adapter.refresh()
-            }
-
-            lifecycleScope.launch {
-                adapter.loadStateFlow.collectLatest { loadStates ->
-                    swipeRefreshLayout.isRefreshing = loadStates.refresh is LoadState.Loading
-                }
-            }
-
-            lifecycleScope.launch {
-                viewModel.photos.collectLatest { pagingData ->
-                    adapter.submitData(pagingData)
-                }
-            }
-
-            adapter.setOnItemClickListener {
-                val loading = Dialog(this@MainActivity)
-                val bindingLoading = DialogLoadingBinding.inflate(layoutInflater)
-                loading.setContentView(bindingLoading.root)
-                loading.setCancelable(false)
-                lifecycleScope.launch {
-                    loading.show()
-                    if (SetWallpaperTask(it.urls.large, wallpaperManager).execute()) {
-                        loading.dismiss()
-                        Toast.makeText(
-                            this@MainActivity,
-                            "The wallpaper has been successfully changed.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        loading.dismiss()
-                        Toast.makeText(
-                            this@MainActivity,
-                            "Error! Please try again later.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-            }
-        }
     }
 }
